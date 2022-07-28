8fcb2a5d47cf4a7ae5213e613c0e2a8f2dcb672d
stndb01 <54304759+stndb01@users.noreply.github.com>
2022-07-28 22:27:14 +0700

    Update index.php

--- test/index.php
+++ test/index.php
@@ -1,4 +1,3 @@
 <?php
-echo "S2I Testing";
-echo "TEST TEXT = Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mattis efficitur nisl sed molestie. Nam quis cursus urna, in sollicitudin libero. Curabitur interdum augue quis erat congue posuere. Proin ipsum quam, consequat in purus quis, pretium lacinia odio. Nunc in faucibus orci. Nunc vehicula bibendum nunc. Cras finibus est non neque placerat facilisis. Maecenas cursus molestie ex sed interdum. Quisque magna purus, hendrerit dapibus diam ut, laoreet suscipit tortor. Cras ornare dolor magna, id fringilla sem imperdiet in. Donec consectetur odio purus, non egestas metus blandit id.";
+echo "BRUH";
 ?> 
